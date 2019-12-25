e481b55709fcbe32cb541e44de6f6d9b2aad4d62
light.pan <373465009@qq.com>
2019-12-25 19:49:42 +0800

    增加接口

--- main.go
+++ main.go
@@ -1,120 +1,57 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
+	"flag"
 	"log"
 	"net/http"
-	"strings"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
-type result struct {
-	State   int    `json:"state"`
-	Message string `json:"msg"`
+type config struct {
+	WorkerID      string `json:"worker_id"`
+	SiteID        string `json:"site_id"`
+	SiteLocation  string `json:"site_location"`
+	SiteIsp       string `json:"site_isp"`
+	Elasticsearch string `json:"elasticsearch"`
 }
+
+var con = &config{}
 
 func main() {
-	http.HandleFunc("/scanner", scanner)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*3, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
 
-func scanner(w http.ResponseWriter, request *http.Request) {
-	res := result{
-		State:   1,
-		Message: "success",
-	}
-	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-
-	if request.Method != http.MethodPost {
-		res.State = 0
-		res.Message = "The request method  must be post"
-		bytes, _ := json.Marshal(res)
-		w.Write(bytes)
-		return
-	}
-	request.ParseMultipartForm(1024)
-	branch := request.PostFormValue("branch")
-	projectURL := request.PostFormValue("project_url")
-	projectName := request.PostFormValue("project_name")
-	projectKey := request.PostFormValue("project_key")
-	sources := request.PostFormValue("sources")
-	language := request.PostFormValue("language")
-	inclusions := request.PostFormValue("inclusions")
-	exclusions := request.PostFormValue("exclusions")
-	globalExclusions := request.PostFormValue("global_exclusions")
-	globalTestExclusions := request.PostFormValue("global_test_exclusions")
-	testInclusions := request.PostFormValue("test_inclusions")
-	testExclusions := request.PostFormValue("test_exclusions")
-	rules := request.PostFormValue("rules")
-
-	p := &processor{
-		Branch:               branch,
-		ProjectURL:           projectURL,
-		ProjectName:          projectName,
-		ProjectKey:           projectKey,
-		Sources:              sources,
-		JSONDir:              "/data/sonar-scanner/json",
-		JarDir:               "/data/sonar-scanner/jar",
-		Language:             language,
-		Rules:                rules,
-		Inclusions:           inclusions,
-		Exclusions:           exclusions,
-		GlobalExclusions:     globalExclusions,
-		GlobalTestExclusions: globalTestExclusions,
-		TestInclusions:       testInclusions,
-		TestExclusions:       testExclusions,
-		SCMDisabled:          "true",
-		KeepReport:           "true",
-		LogLevel:             "trace",
-		Command:              "/data/sonar-scanner/bin/sonar-scanner",
+	r := mux.NewRouter()
+	r.HandleFunc("/admin/config", setConfig).Methods(http.MethodPut)
+	r.HandleFunc("/admin/config", getConfig).Methods(http.MethodGet)
+	r.HandleFunc("/tasks/{id:[0-9]+}", scanner).Methods(http.MethodPut)
+	r.HandleFunc("/tasks/{id:[0-9]+}", deleteTask).Methods(http.MethodDelete)
+	srv := &http.Server{
+		Addr:         "0.0.0.0:8080",
+		WriteTimeout: time.Second * 3,
+		ReadTimeout:  time.Second * 3,
+		IdleTimeout:  time.Second * 30,
+		Handler:      r,
 	}
 
-	if strings.TrimSpace(p.Branch) == "" {
-		p.Branch = "master"
-	}
-	if strings.TrimSpace(p.Sources) == "" {
-		p.Sources = "."
-	}
-	lags := strings.Split(p.Language, ",")
-	if strings.TrimSpace(p.Language) != "" {
-		for _, language := range lags {
-			if !inArray(languages, language) {
-				res.State = 0
-				res.Message = "The languages should be in " + strings.Join(languages, ",")
-				bytes, _ := json.Marshal(res)
-				w.Write(bytes)
-				return
-			}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
 		}
-	}
-	if strings.TrimSpace(p.ProjectURL) == "" {
-		res.State = 0
-		res.Message = "The project_url must be not null"
-		bytes, _ := json.Marshal(res)
-		w.Write(bytes)
-		return
-	}
-	if strings.TrimSpace(p.ProjectName) == "" {
-		res.State = 0
-		res.Message = "The project_name must be not null"
-		bytes, _ := json.Marshal(res)
-		w.Write(bytes)
-		return
-	}
-	if strings.TrimSpace(p.ProjectKey) == "" {
-		p.ProjectKey = p.ProjectName
-	}
-
-	go p.scanner()
-	bytes, _ := json.Marshal(res)
-	w.Write(bytes)
-}
-
-func inArray(arr []string, value string) bool {
-	for _, v := range arr {
-		if v == value {
-			return true
-		}
-	}
-	return false
+	}()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	<-c
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	srv.Shutdown(ctx)
+	log.Println("shutting down")
+	os.Exit(0)
 }
